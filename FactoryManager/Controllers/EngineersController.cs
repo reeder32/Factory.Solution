93e03e9154854f5e35e4d0d5ad6fc7b7b76f3b09
Nicholas Reeder <nickreeder32@gmail.com>
2021-05-28 10:20:09 -0700

    :truck: create view and controller added

--- FactoryManager/Controllers/EngineersController.cs
+++ FactoryManager/Controllers/EngineersController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using FactoryManager.Models;
 
 namespace FactoryManager.Controllers
@@ -18,6 +19,23 @@ namespace FactoryManager.Controllers
     {
       List<Engineer> engineers = _db.Engineers.ToList();
       return View(engineers);
+    }
+     public ActionResult Create()
+    {
+      ViewBag.MachineId = new SelectList(_db.Machines, "MachineId", "ModelName");
+      return View();
+    }
+    [HttpPost]
+     public ActionResult Create(Engineer engineer, int MachineId)
+    {
+      _db.Engineers.Add(engineer);
+      _db.SaveChanges();
+      if (MachineId != 0)
+      {
+        _db.EngineerMachines.Add(new EngineerMachine() { MachineId = MachineId, EngineerId = engineer.EngineerId });
+      }
+      _db.SaveChanges();
+      return RedirectToAction("Index");
     }
   }
 }
