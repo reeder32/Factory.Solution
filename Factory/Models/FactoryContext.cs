fa7e018a572e01803fd0db44a9525a7ce0f3d5d2
Nicholas Reeder <nickreeder32@gmail.com>
2021-05-28 08:51:40 -0700

    :tada: init files and folders

---
+++ Factory/Models/FactoryContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Factory.Models
+{
+  public class FactoryContext : DbContext
+  {
+    public virtual DbSet<Machine> Machines { get; set; }
+    public DbSet<Enginner> Engineers { get; set; }
+    public DbSet<EngineerMachine> EngineerMachine { get; set; }
+    public FactoryContext(DbContextOptions options) : base(options) { }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      optionsBuilder.UseLazyLoadingProxies();
+    }
+  }
+}
